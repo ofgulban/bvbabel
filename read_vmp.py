7cef35843d591de19cc11de476e2a357b091562a
ofgulban <farukgulban@gmail.com>
2021-03-24 11:27:01 +0100

    wip

--- read_vmp.py
+++ read_vmp.py
@@ -5,7 +5,7 @@ import numpy as np
 import nibabel as nb
 
 FILE = "/home/faruk/Documents/test_bvbabel/aseg_float_aligned.vmp"
-OUT_NII = "/home/faruk/Documents/test_bvbabel/aseg_float_aligned.nii.gz"
+OUT_NII = "/home/faruk/Documents/test_bvbabel/aseg_float_aligned_bvbabel.nii.gz"
 
 
 # =============================================================================
@@ -192,8 +192,8 @@ with open(FILE, 'rb') as reader:
         # Time course values associated with component "c"
         # NOTE(Faruk): I dont really understand the use-case of this. I need an
         # example VMP that has this field to test it.
-        header["ComponentTimeCourseValues"] = []
         if header["NrOfTimePoints"] > 0:
+            header["ComponentTimeCourseValues"] = []
             for i in range(header["NrOfSubMaps"]):
                 data = np.zeros(header["NrOfTimePoints"])
                 for j in range(header["NrOfTimePoints"]):
@@ -204,11 +204,28 @@ with open(FILE, 'rb') as reader:
         if header["NrOfComponentParams"] > 0:
             header["ComponentTimeCourseParams"] = []
             for i in range(header["NrOfComponentParams"]):
-                data = read_variable_length_string(reader)
-                header["NameOfParam"] = data
-                # for j in range(header["NrOfSubMaps"]):
-                #     header["ComponentParamValues"]
+                name = read_variable_length_string(reader)
+                header[name] = []
+                for j in range(header["NrOfSubMaps"]):
+                    data, = struct.unpack('<f', reader.read(4))
+                    header[name].append(data)
 
+    # -------------------------------------------------------------------------
+    # Read VMP image data
+    # -------------------------------------------------------------------------
+    # NOTE(Faruk): I think the VMP data is separated into is own header + image
+    # structre. Therefore, I might need to handle this within component loops
+    VMP_resolution = header["Resolution"]
+    DimX = (header["XEnd"] - header["XStart"]) // VMP_resolution
+    DimY = (header["YEnd"] - header["YStart"]) // VMP_resolution
+    DimZ = (header["ZEnd"] - header["ZStart"]) // VMP_resolution
+    DimT = header["NrOfSubMaps"]
+    data_img = np.zeros(DimZ * DimY * DimX * DimT)
+    for i in range(data_img.size):
+        data_img[i], = struct.unpack('<f', reader.read(4))
+    data_img = np.reshape(data_img, (DimZ, DimY, DimX, DimT))
+    data_img = np.transpose(data_img, (0, 2, 1, 3))  # BV to Tal
+    data_img = data_img[::-1, ::-1, ::-1, :]  # Flip BV axes
 
 # Print header information
 for key, value in header.items():
@@ -221,5 +238,5 @@ for key, value in header.items():
         print(key, ":", value)
 
 # Test output data
-# img_nii = nb.Nifti1Image(data, affine=np.eye(4))
-# nb.save(img_nii, OUT_NII)
+img_nii = nb.Nifti1Image(data_img, affine=np.eye(4))
+nb.save(img_nii, OUT_NII)
