251f9aac87b353ef50c3a00b437fafcc552a831f
ofgulban <farukgulban@gmail.com>
2021-07-04 13:22:59 +0200

    Add GTC read write

--- bvbabel/__init__.py
+++ bvbabel/__init__.py
@@ -3,6 +3,7 @@
 import bvbabel.vmr
 import bvbabel.vmp
 import bvbabel.vtc
+import bvbabel.gtc
 
 import pkg_resources
 __version__ = pkg_resources.require("bvbabel")[0].version
---
+++ examples/read_gtc_export_nifti.py
@@ -0,0 +1,24 @@
+"""Read Brainvoyager gtc and export nifti."""
+
+import os
+import numpy as np
+import nibabel as nb
+import bvbabel
+from pprint import pprint
+
+FILE = "/home/faruk/Documents/test_bvbabel/gtc/gtc_test.gtc"
+
+# =============================================================================
+# Load file
+header, data_img = bvbabel.gtc.read_gtc(FILE)
+
+# See header information
+pprint(header)
+
+# Export nifti
+basename = FILE.split(os.extsep, 1)[0]
+outname = "{}_bvbabel.nii.gz".format(basename)
+img = nb.Nifti1Image(data_img, affine=np.eye(4))
+nb.save(img, outname)
+
+print("Finished.")
