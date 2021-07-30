35b48e7c2648a3cdf4484b92c32e8a3a9e225f90
ofgulban <farukgulban@gmail.com>
2021-07-30 15:24:19 +0200

    Iterate CMF computation algo.

--- wip/smp_compute_cortical_magnification.py
+++ wip/smp_compute_cortical_magnification.py
@@ -13,6 +13,10 @@ FILE_SMP = "/home/faruk/Documents/test_bvbabel/srf_smp/maps.smp"
 VMR_IMAGE_DIMS = 512  # Stands for e.g. 512 x 512 x 512, or 256 x 256 x 256
 VMR_VOXEL_DIMS = 0.4  # Stands for e.g 0.4 x 0.4 x 0.4 mm^3 ot 1 x 1 x 1 mm^3
 
+# TODO: More information might be needed to compute visual angle degrees to
+# have the right numerical range in CMF computation.
+#   CMF = "millimeters of cortical surface" / "degree of visual angle"
+
 # =============================================================================
 # Load files
 header_srf, data_srf = bvbabel.srf.read_srf(FILE_SRF)
@@ -22,62 +26,68 @@ header_smp, data_smp = bvbabel.smp.read_smp(FILE_SMP)
 vtx = data_srf["vertices"]
 # Get vertex neighbors (python list)
 nbr = data_srf["vertex neighbors"]
-# Get eccentricity & polar angle
-print(header_smp["Map"][4]["Name"])
-map_polar = data_smp[:, 4]
-map_eccen = data_smp[:, 5]
+# Get PRF mapping visual field c & y coordinates
+print(header_smp["Map"][1]["Name"])
+print(header_smp["Map"][2]["Name"])
+prf_xy = data_smp[:, 1:3]
 
 # -----------------------------------------------------------------------------
+print("Computing cortical magnification factors...")
 # Prepare useful variables
 nr_vtx = header_srf["Nr vertices"]
-map_new = np.zeros(nr_vtx)
-
-# Convert eccentricity and polar angle to carthesian coordinates
-cart = np.zeros([nr_vtx, 2])
-cart[:, 0] = map_eccen * np.cos(map_polar)
-cart[:, 1] = map_eccen * np.sin(map_polar)
+map_cmf = np.zeros(nr_vtx)
 
 # Compute cortical magnification for each vertex
 for v in range(nr_vtx):
     if map[v] != 0:
         # ---------------------------------------------------------------------
         # Compute distance in space between neighboring vertices
-        dist_sum = 0
+        cmf_sum = 0
+        n_count = 0
         for n in nbr[v][1:]:  # Loop over neighbor vertices
-            dist = np.linalg.norm(vtx[v, :] - vtx[n, :])  # Euclidean distance
-            dist_sum += dist  # Add distance between each neighbor vertex
-        dist_avg_space = dist_sum / nbr[v][0]  # Normalize with nr of neighbors
+            # Compute vertex to vertex mesh distance
+            dist_cortex = np.linalg.norm(vtx[v, :] - vtx[n, :])
+            # Convert vertex to vertex mesh distance to millimeters
+            dist_cortex *= (VMR_IMAGE_DIMS / 256) * VMR_VOXEL_DIMS
 
-        # Standardize with voxel size and mesh to vmr scale. When properly set
-        # this scaling converts vertex-wise distance measurement units to
-        # millimeters.
-        dist_avg_space *= (VMR_IMAGE_DIMS / 256) * VMR_VOXEL_DIMS
+            # Compute vertex to vertex PRF xy coordinates distance
+            dist_vfield = np.linalg.norm(prf_xy[v, :] - prf_xy[n, :])
+            # TODO: Convert Vertex to vertex PRF xy distance to vis. angle deg.
 
-        # ---------------------------------------------------------------------
-        # Compute distance in visual field between neighboring vertices
-        dist_sum = 0
-        for n in nbr[v][1:]:
-            dist = np.linalg.norm(cart[v, :] - cart[n, :])
-            dist_sum += dist
-        dist_avg_visfield = dist_sum / nbr[v][0]
+            # Compute cortical magnification factor (CMF)
+            # NOTE: CMF = "mm of cortical surface" / "degree of visual angle"
+            if dist_vfield > 0:
+                cmf_sum += dist_cortex / dist_vfield
+                n_count += 1
 
-        # ---------------------------------------------------------------------
-        # Compute cortical magnification factor (CMF)
-        cmf = dist_avg_space / dist_avg_visfield
-        map_new[v] = cmf
+        # Normalize cumulative CMF with the number of non-zero neighbours
+        if cmf_sum > 0:
+            cmf = cmf_sum / n_count
+
+        # Put the vertex-wise average CMF into smp map format
+        map_cmf[v] = cmf
     else:
-        map_new[v] = 0
+        map_cmf[v] = 0
 
 # -----------------------------------------------------------------------------
 # Prepare new SMP map
 header_smp["Nr maps"] += 1
 header_smp["Map"].append(copy(header_smp["Map"][4]))
-header_smp["Map"][6]["Name"] = "Cortical Magnification Factor, UseThreshMap: R"
-header_smp["Map"][6]["Threshold min"] = 0.001
-header_smp["Map"][6]["Threshold max"] = 10.
-header_smp["Map"][6]["LUT file"] = "default_v21_inv.olt"
+header_smp["Map"][-1]["Name"] = "CMF, UseThreshMap: R"
+header_smp["Map"][-1]["Threshold min"] = 0.001
+header_smp["Map"][-1]["Threshold max"] = 5.
+header_smp["Map"][-1]["LUT file"] = "default_v21_inv.olt"
+data_smp = np.hstack([data_smp, map_cmf[:, None]])
 
-data_smp = np.hstack([data_smp, map_new[:, None]])
+# Add reciprocal of CMF as it linearly increases with eccentricity
+header_smp["Nr maps"] += 1
+header_smp["Map"].append(copy(header_smp["Map"][4]))
+header_smp["Map"][-1]["Name"] = "CMF reciprocal, UseThreshMap: R"
+header_smp["Map"][-1]["Threshold min"] = 0.001
+header_smp["Map"][-1]["Threshold max"] = 1.5
+header_smp["Map"][-1]["LUT file"] = "default_v21_inv.olt"
+map_cmf[map_cmf > 0] = 1 / map_cmf[map_cmf > 0]  # Reciprocal of non-zeros
+data_smp = np.hstack([data_smp, map_cmf[:, None]])
 
 # Save SMP
 basename = FILE_SMP.split(os.extsep, 1)[0]
