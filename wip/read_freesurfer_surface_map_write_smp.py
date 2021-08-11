871f27584ce468e18715f6a82d5081abd8805369
Omer Faruk Gulban <farukgulban@gmail.com>
2021-08-11 18:52:53 +0200

    Add freesurfer surface map (mgh) conversion script

---
+++ wip/read_freesurfer_surface_map_write_smp.py
@@ -0,0 +1,33 @@
+"""Convert Freesurfer *.pct.mgh file into BrainVoyager SMP format."""
+
+import os
+import numpy as np
+import nibabel as nb
+import bvbabel
+
+# NOTE: Full path to `<subjid>/surf/?h.w-g.pct.mgh` file. This file is a
+# surface map that has gray to white matter signal intensity ratios for each
+# vertex (ref: <https://surfer.nmr.mgh.harvard.edu/fswiki/pctsurfcon>)
+FILE = "/home/faruk/Documents/temp_bvbabel_mgh/lh.w-g.pct.mgh"
+
+# -----------------------------------------------------------------------------
+# Read Freesurfer `*.w-g.pct.mgh` surface map
+mgh = nb.load(FILE)
+mgh_data = np.squeeze(np.asarray(mgh.dataobj))
+nr_vertices = mgh_data.shape[0]
+
+# Generate dummy SMP file
+smp_header, smp_data = bvbabel.smp.generate_smp(nr_vertices=nr_vertices)
+
+# Update some fields with mgh information
+header["Map"][m]["Threshold min"] = np.min(mgh_data)
+header["Map"][m]["Threshold max"] = np.max(mgh_data)
+
+# Determine output name
+basename = FILE.split(os.extsep, 1)[0]
+outname = "{}_bvbabel.smp".format(basename)
+
+# Save SMP file while using the freesurfer MGH data
+bvbabel.smp.write_smp(outname, smp_header, mgh_data[:, None])
+
+print('Finished.')
