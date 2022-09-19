e33457d007a4bf7d2fac7c23f93113df9cb9f3e4
ofgulban <farukgulban@gmail.com>
2022-09-19 16:27:39 +0200

    Change data structure to more closely follow `read_VOI` function Main difference in that the predictor names and colors are now associated directly with the predictor weights. I have done this because I think it is safer to keep the predictor descriptors together with the weight information. I think this structure would allow us to more flexibly add or subtract preditors and write back a new SDM file in the future. In addition, I have shortened the code a bit. Particularly the part covering scientific notation and concatenated column values.

--- bvbabel/sdm.py
+++ bvbabel/sdm.py
@@ -36,8 +36,8 @@ def read_sdm(filename):
 
     # SDM header
     header = dict()
-    header_rows = 7  # = no of rows without empty lines
-    for line in lines[0:(header_rows-2)]:
+    header_rows = 5  # Nr of rows without empty lines
+    for line in lines[0:header_rows]:
         content = line.split(":")
         content = [i.strip() for i in content]
         if content[1].isdigit():
@@ -45,52 +45,40 @@ def read_sdm(filename):
         else:
             header[content[0]] = content[1]
 
-    colors = lines[5].split(" ")
-    colors = [c.strip(" ") for c in colors]
-    colors2 = []
-    for c in range(len(colors)):
-        if colors[c].isdigit():
-            colors2.append(int(colors[c]))
-    rgb = np.zeros((header["NrOfPredictors"], 3), dtype=np.int32)
-    for c in range(len(colors2)):
-        grouping = divmod(c, 3)
-        rgb[grouping[0]][grouping[1]] = colors2[c]
-    header["Colors"] = rgb
+    # -----------------------------------------------------------------------------
+    # SDM data columnns
+    nr_cols = header["NrOfPredictors"]
+    nr_rows = header["NrOfDataPoints"]
 
-    names = lines[6]
-    from_index = 0
-    namesarr = []
-    for p in range(header["NrOfPredictors"]):
-        from_index = names.find('\"', from_index)
-        end_index = names.find('\"', from_index+1)
-        pname = names[from_index:end_index]
-        namesarr.append(pname.replace("\"", ""))
-        from_index = (end_index+1)
-        p += 1
-    header["PredictorNames"] = namesarr
+    # Parse column colors (RGB integer triplets for each column)
+    col_rgb_row = 5
+    colors = [i for i in lines[col_rgb_row].split(" ") if i.isdigit()]
+    col_rgb = list()
+    for i in range(0, nr_cols*3, 3):
+        col_rgb.append([int(colors[i]), int(colors[i+1]), int(colors[i+2])])
 
-    # SDM data (NrOfDataPoints x NrOfPredictors)
-    data = []
-    for r, line in enumerate(lines[header_rows:]):
-        values = line.split(" ")
-        values = [v.strip() for v in values]
-        values2 = []
-        for v in range(len(values)):
-            p = values[v]
-            if len(p) > 0:
-                # Detect concatenated numbers in *_3DMC.sdm files, such as
-                # '0.0310625-0.000387509'
-                sign = p.rfind("-", 1)
-                if sign > -1:
-                    # Detect scientific notation in *_3DMC.sdm files, such as
-                    # '5.806e-05'
-                    if p.rfind("e") > -1:
-                        values2.append(float(p))
-                    else:
-                        values2.append(float(p[:sign]))
-                        values2.append(float(p[sign:]))
-                else:
-                    values2.append(float(p))
-        data.append(values2)
+    # Parse column names
+    col_name_row = 6
+    col_name = [i.strip("\"") for i in lines[col_name_row].split(" ") if i]
+
+    # Parse column data
+    col_values_row = 7
+    col_values = np.zeros((nr_rows, nr_cols))
+    for r, line in enumerate(lines[col_values_row:]):
+        temp = line.replace("e-", "!@#$%")  # Preserve scientific notation
+        temp = temp.replace("-", " -")  # Separate concatenated columns
+        temp = temp.replace("!@#$%", "e-")  # Restore scientific notation
+        temp = temp.split(" ")
+        col_values[r, :] = [float(i.strip()) for i in temp if i]
+
+    # -----------------------------------------------------------------------------
+    # Reorganize column information
+    data = list()
+    for i in range(nr_cols):
+        temp = dict()
+        temp["NameOfPredictor"] = col_name[i]
+        temp["ColorOfPredictor"] = col_rgb[i]
+        temp["ValuesOfPredictor"] = col_values[:, i]
+        data.append(temp)
 
     return header, data
