0f8c5b8b7a44b01d7503a3a6909ebd3cfc99438d
Omer Faruk Gulban <farukgulban@gmail.com>
2023-04-04 19:54:50 +0200

    Add optional reorientation example lines

--- examples/read_nifti_write_vmr.py
+++ examples/read_nifti_write_vmr.py
@@ -1,22 +1,31 @@
 """Read Nifti write BrainVoyager VMR and V16 (anatomical image) files."""
-
 import os
 import bvbabel
 import nibabel as nb
 import numpy as np
 import pprint
 
-FILE = "/home/faruk/data2/DATA-AHEAD/MNI_coregistrations/Ahead_brain_122017_Bielschowsky-mni2009b.nii.gz"
+FILE = "/home/faruk/data2/DATA-AHEAD/3D_reconstructions/Ahead_brain_122017_Bieloschowsky-interpolated.nii.gz"
+
+SUFFIX = "bvbabel"
 
 # =============================================================================
 # Load Nifti
 nii = nb.load(FILE)
 nii_data = np.nan_to_num(nii.get_fdata(), nan=0.)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# # (Optional - Use with caution!) Change the image orientation in BV
+# nii_data = np.transpose(nii_data, [0, 2, 1])
+
+# # (Optional - Use with caution!) Flip directions
+# nii_data = nii_data[::-1, ::-1, ::-1]
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 dims = nii_data.shape
 
 print("\n" + "="*79 + "\nNIFTI HEADER\n" + "="*79)
 print(nii.header)
-print(np.percentile(nii_data[nii_data != 0], [0, 100]))
 
 # -----------------------------------------------------------------------------
 # Create V16
@@ -32,16 +41,16 @@ v16_data = v16_data / (thr_max - thr_min) * 65535
 v16_data = np.asarray(v16_data, dtype=np.ushort)
 
 # Update V16 headers
-v16_header["DimX"] = nii.header["dim"][2]
-v16_header["DimY"] = nii.header["dim"][3]
-v16_header["DimZ"] = nii.header["dim"][1]
+v16_header["DimX"] = dims[1]  # nii.header["dim"][2]
+v16_header["DimY"] = dims[2]  # nii.header["dim"][3]
+v16_header["DimZ"] = dims[0]  # nii.header["dim"][1]
 
 print("\n" + "="*79 + "\nV16 HEADER\n" + "="*79)
 pprint.pprint(v16_header)
 
 # Save V16
 basename = FILE.split(os.extsep, 1)[0]
-outname = "{}_bvbabel.v16".format(basename)
+outname = "{}_{}.v16".format(basename, SUFFIX)
 bvbabel.v16.write_v16(outname, v16_header, v16_data)
 
 # -----------------------------------------------------------------------------
@@ -62,9 +71,9 @@ vmr_header["ColDirX"] = 0.0
 vmr_header["ColDirY"] = 0.0
 vmr_header["ColDirZ"] = 0.0
 vmr_header["CoordinateSystem"] = 0
-vmr_header["DimX"] = nii.header["dim"][2]
-vmr_header["DimY"] = nii.header["dim"][3]
-vmr_header["DimZ"] = nii.header["dim"][1]
+vmr_header["DimX"] = dims[1]  # nii.header["dim"][2]
+vmr_header["DimY"] = dims[2]  # nii.header["dim"][3]
+vmr_header["DimZ"] = dims[0]  # nii.header["dim"][1]
 vmr_header["File version"] = 4
 vmr_header["FoVCols"] = 0.0
 vmr_header["FoVRows"] = 0.0
@@ -102,7 +111,7 @@ print("\n" + "="*79 + "\nVMR HEADER\n" + "="*79)
 pprint.pprint(vmr_header)
 
 # Save VMR
-outname = "{}_bvbabel.vmr".format(basename)
+outname = "{}_{}.vmr".format(basename, SUFFIX)
 bvbabel.vmr.write_vmr(outname, vmr_header, vmr_data)
 
 print("\nFinished.\n")
