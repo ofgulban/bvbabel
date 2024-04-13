57db930e1dcf305a4eba6f2774caf0736bc614db
ofgulban <farukgulban@gmail.com>
2024-04-13 12:17:45 +0200

    Improve fmr to nifti export to solve #53

--- examples/read_fmr_export_nifti.py
+++ examples/read_fmr_export_nifti.py
@@ -1,11 +1,11 @@
-"""Read BrainVoyager FMR file format."""
+"""Read BrainVoyager FMR file format and exxport it as Nifti."""
 
 import os
 import numpy as np
 import nibabel as nb
 import bvbabel
 
-FILE = "/home/faruk/Documents/test_bvbabel/fmr/nifti_converted.fmr"
+FILE = "/Users/faruk/data/temp-kenshu/S05_ses1_run01_SCSA_3DMCTS_THPGLMF6c_ref.fmr"
 
 # =============================================================================
 # Load fmr
@@ -17,16 +17,17 @@ outname = "{}_bvbabel.nii.gz".format(basename)
 
 # Export nifti (assign an identity matrix as affine with default header)
 img = nb.Nifti1Image(data, affine=np.eye(4))
+
+# Carry over equivalent header information
+img.header["pixdim"][1] = header['InplaneResolutionX']
+img.header["pixdim"][2] = header['InplaneResolutionY']
+img.header["pixdim"][3] = header['SliceThickness']
+img.header["pixdim"][4] = float(header['TR']) / 1000  # ms to s
+img.affine[0, 0] *= img.header["pixdim"][1]
+img.affine[1, 1] *= img.header["pixdim"][2]
+img.affine[2, 2] *= img.header["pixdim"][3]
+
+# Save
 nb.save(img, outname)
-
-# -----------------------------------------------------------------------------
-# NOTE[Faruk]: I need to think about exporting nifti with a header that matches
-# the fmr header fields
-
-# Export nifti (Pull affine matrix from fmr header)
-# affine = header["Transformation information"]["Transformation matrix"]
-# img = nb.Nifti1Image(data, affine=affine)
-# nb.save(img, outname)
-# -----------------------------------------------------------------------------
 
 print("Finished.")
