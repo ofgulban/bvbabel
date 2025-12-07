17bad6bd89955b5e2f8adb1ed78462ae7907a6b5
ofgulban <farukgulban@gmail.com>
2025-12-07 22:21:53 +0100

    Add offset example for VTC exports that will fit on VMR nifti exports

--- examples/read_vtc_export_nifti.py
+++ examples/read_vtc_export_nifti.py
@@ -23,7 +23,22 @@ data = data[::-1, ::-1, ::-1, :]
 # Export nifti
 basename = FILE.split(os.extsep, 1)[0]
 outname = "{}_bvbabel.nii.gz".format(basename)
-img = nb.Nifti1Image(data, affine=np.eye(4))
+
+# Create affine
+affine = np.eye(4) * header["VTC resolution relative to VMR (1, 2, or 3)"]
+
+# -----------------------------------------------------------------------------
+# (Optional) Insert translation values when necessary
+# NOTE: These need to be figured out for each necessary transpose and flip
+# -----------------------------------------------------------------------------
+# TARGET_VMR_DIMENSION = 256 # Often 256, but can also be an integer multiple
+# affine[1, 3] = TARGET_VMR_DIMENSION - header["XEnd"] + 1.5
+# affine[2, 3] = TARGET_VMR_DIMENSION - header["YEnd"] + 1.5
+# affine[0, 3] = TARGET_VMR_DIMENSION - header["ZEnd"] + 1.5
+
+# -----------------------------------------------------------------------------
+# Save
+img = nb.Nifti1Image(data, affine=affine)
 nb.save(img, outname)
 
 print("Finished.")
